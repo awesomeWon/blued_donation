478d0e65c61a4a20e1bf14c08ed13518ee1d31d6
tk <constwon111@gmail.com>
2022-03-16 13:28:43 +0900

    a

--- server/util/pool.ts
+++ server/util/pool.ts
@@ -2,13 +2,13 @@ export {};
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable import/no-unresolved */
-import * as mysql from "mysql2";
+import * as mysql from "mysql";
 import { dbConfig } from "../config";
 
 interface DBresult {
     status: string;
     data: Array<JSON>;
 }
-const pool = mysql.createPool(dbConfig).promise();
+const pool = mysql.createPool(dbConfig);
 
 export { pool, DBresult };
