ada3aaccd7578c16783c442df93ebffdb760c2c6
tk <constwon111@gmail.com>
2022-03-18 01:46:22 +0900

    a

--- client/next.config.js
+++ client/next.config.js
@@ -1,35 +1,61 @@
 const API_KEY = process.env.API_KEY;
 
-module.exports = {
-    reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: "/old-blog/:path*",
-                destination: "/new-sexy-blog/:path*",
-                permanent: false,
-            },
-        ];
-    },
 
-    async rewrites() {
-        return [
-            // {
-            //     source: "/api/:path*",
-            //     destination: "http://localhost:5000/api/:path*",
-            // },
-            // {
-            //     destination: "/new-sexy-blog/:path*",
-            //     source: "/api/oAuth/kakao",
-            // },
-            // {
-            //   source: "/api/oAuth/kakao/logout",
-            //   destination: `http://localhost:5000/oAuth/kakao/logout`,
-            // },
-            // {
-            //   source: "/api/oAuth/user",
-            //   destination: `http://localhost:5000/oAuth/user`,
-            // }
-        ];
-    },
+{
+    "distDir": "build",
+    "exportTrailingSlash": true,
+    "assetPrefix": "https://~~~"
+  }
+
+module.exports = {
+    "distDir": "build",
+    "exportTrailingSlash": true,
+
+    // exportPathMap: async function (
+    //     defaultPathMap,
+    //     { dev, dir, outDir, distDir : build, buildId }
+    //   ) {
+    //     return {
+    //       '/': { page: '/' },
+    //       '/about': { page: '/about' },
+    //       '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+    //       '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+    //       '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    //     }
+    //   },
+
+
+    // reactStrictMode: true,
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: "/old-blog/:path*",
+    //             destination: "/new-sexy-blog/:path*",
+    //             permanent: false,
+    //         },
+    //     ];
+    // },
+ 
+
+
+    // async rewrites() {
+    //     return [
+    //         // {
+    //         //     source: "/api/:path*",
+    //         //     destination: "http://localhost:5000/api/:path*",
+    //         // },
+    //         // {
+    //         //     destination: "/new-sexy-blog/:path*",
+    //         //     source: "/api/oAuth/kakao",
+    //         // },
+    //         // {
+    //         //   source: "/api/oAuth/kakao/logout",
+    //         //   destination: `http://localhost:5000/oAuth/kakao/logout`,
+    //         // },
+    //         // {
+    //         //   source: "/api/oAuth/user",
+    //         //   destination: `http://localhost:5000/oAuth/user`,
+    //         // }
+    //     ];
+    // },
 };
