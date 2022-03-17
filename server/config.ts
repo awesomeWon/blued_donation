70079161e287570228b01bf2fe725ce0a412e1f6
tk <constwon111@gmail.com>
2022-03-17 23:44:26 +0900

    a

--- server/config.ts
+++ server/config.ts
@@ -11,6 +11,7 @@
 const dbConfig = {
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
+
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
